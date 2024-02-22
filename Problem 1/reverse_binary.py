88408e92c3683c8a3b51c272696fdf119718ab0f
ota1953 <157063491+ota1953@users.noreply.github.com>
2024-02-22 10:24:12 -0600

    Please work

--- Problem 1/reverse_binary.py
+++ Problem 1/reverse_binary.py
@@ -1,15 +1,17 @@
 """
 Complete the following python code to print the reverse binary representation of positive integer number entered by the user.
 
-Name:
-Lab Time:
+Name: OWEN ANDERSON
+Lab Time: THUR 2:00-3:15
 
 """
 
 
 def reverse_binary():
     user_num = int(input())
-    # YOUR CODE HERE
+    if user_num > 0:
+        
+
 
 if __name__ == "__main__":
     reverse_binary()
--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -1,14 +1,36 @@
 """
 Complete the following python code to print the password entered by the user with the modifications described in the readme
 
-Name:
-Lab Time:
+Name: Owen Anderson
+Lab Time: THUR 2:00 - 3:15
 """
 
 def password_mod():
-    word = input()
+    word = input(str())
     password = ''
-    # Type your code here.
+    word_list = list(word)
 
+    index = 0
+    while index < len(word_list):
+        if word_list[index] == 'i':
+            word_list[index] = '1'
+
+        if word_list[index] =='a':
+            word_list[index] = '@'
+
+        if word_list[index] == 'm':
+            word_list[index] = 'M'
+
+        if word_list[index] == 'B':
+            word_list[index] = '8'
+
+        if word_list[index] == 's':
+            word_list[index] = '$'
+        
+        password += word_list[index]
+        index += 1
+
+        
+    print(password+"!")
 if __name__ == "__main__":
     password_mod()
