041365a54307260a45b92818178a795d77f50b03
ota1953 <157063491+ota1953@users.noreply.github.com>
2024-02-22 14:38:40 -0600

    Is it working?

--- Problem 4/reverse_string.py
+++ Problem 4/reverse_string.py
@@ -1,12 +1,19 @@
 """
 Complete the following python code to reverse the string entered by the user.
 
-Name: 
-Lab Time:
+Name: Owen Anderson
+Lab Time: THUR 2:00 - 3:15
 """
 
 def reverse_string():
-    # YOUR CODE HERE
+    word_input = input()
+    reverse = ''
+    while len(word_input) > 0 and word_input != "done" and word_input != "Done" and word_input != "d":
+        print(word_input[::-1])
+        word_input = input()
+    pass
+
+
     
 
 if __name__ == "__main__":
