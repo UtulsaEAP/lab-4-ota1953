bda3b67794447479256004581ffe772c19d3b53c
ota1953 <157063491+ota1953@users.noreply.github.com>
2024-02-22 12:28:52 -0600

    Second down

--- Problem 3/inc_5.py
+++ Problem 3/inc_5.py
@@ -1,14 +1,18 @@
 """
 Complete the following python code to print all numbers between the two values incrementing by 5 from the initial value to the final value. The initial value and final value are entered by the user. If the final value is less than the initial value, print "Second integer can't be less than the first.
 
-Name:
-Lab Time:
+Name: Owen Anderson
+Lab Time: THUR 2:00-3:15
 """
 
 def inc_5():
-    # Write your code here
-    
-
+    first_num = int(input())
+    sec_num = int(input())
+    if first_num > sec_num:
+        print("Second integer can't be less than the first.")
+    else:
+        for i in range(first_num, sec_num, 5):
+            print(i, end=' ')
 
 
 if __name__ == '__main__':
